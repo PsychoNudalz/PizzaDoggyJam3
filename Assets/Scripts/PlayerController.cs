2ef9bddeb2cdb62e8a0d86c740843b1f040f8a73
Anson <anson.aw0618@gmail.com>
2025-11-02 20:14:14 +1100

    Anson: Player input lock possible

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,63 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField]
+    PlayerInput playerInput;
+    bool inputLock = false;
+
+
+    public static PlayerController Instance;
+    void Awake()
+    {
+        playerInput = GetComponent<PlayerInput>();
+
+        if (Instance)
+        {
+            Destroy(Instance.gameObject);
+        }
+
+        Instance = this;
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public static void LockPlayerInput(bool lockInput)
+    {
+        Instance.inputLock = lockInput;
+        Instance.playerInput.enabled = !lockInput;
+    }
+
+    public static void DisablePlayerInput_Static(float duration)
+    {
+        Instance?.DisablePlayerInput(duration);
+    }
+
+    public void DisablePlayerInput(float duration)
+    {
+        StartCoroutine(WaitDisablePlayerInput(duration));
+    }
+
+
+
+
+    IEnumerator WaitDisablePlayerInput(float duration)
+    {
+        LockPlayerInput(true);
+        yield return new WaitForSeconds(duration);
+        LockPlayerInput(false);
     }
 }
