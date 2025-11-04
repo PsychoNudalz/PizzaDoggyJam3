93a428e0954b5ef39c5c68b94c4d7433eaf84584
Anson <anson.aw0618@gmail.com>
2025-11-04 19:12:07 +1100

    Anson: video now works on webgl, disable mission

---
+++ Assets/Scripts/VideoWorkAround.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+[RequireComponent(typeof(VideoPlayer))]
+public class VideoWorkAround : MonoBehaviour
+{
+    [SerializeField]
+    string videoName = "Upper_SA";
+    VideoPlayer videoPlayer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        videoPlayer = GetComponent<VideoPlayer>();
+
+        if (videoPlayer)
+        {
+            if (videoPlayer.clip)
+            {
+                videoName = videoPlayer.clip.name;
+            }
+            videoPlayer.url = System.IO.Path.Combine (Application.streamingAssetsPath,videoName+"_SA.mp4");
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
